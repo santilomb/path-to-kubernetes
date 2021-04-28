f80e99cef14d4b01e174f338ba2a438e684e7a95
Santiago Lombardi <santiago.lombardi@gmail.com>
2021-04-28 09:31:24 -0300

    Update HelloWorldApplication.java

--- src/java/helloworld/src/main/java/com/example/helloWorld/HelloWorldApplication.java
+++ src/java/helloworld/src/main/java/com/example/helloWorld/HelloWorldApplication.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @CrossOrigin
 @SpringBootApplication
